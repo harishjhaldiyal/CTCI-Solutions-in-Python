9cee4a81bc971a35a7cbc31100f41f5b9ea5131c
Harish Jhaldiyal <31455549+harishjhaldiyal@users.noreply.github.com>
2018-06-24 22:59:21 +0800

    4.1

---
+++ 4.1.py
@@ -0,0 +1,43 @@
+"""
+graph: a -> b -> c -> d
+
+inputs: 
+
+answer: b -> c -> d
+
+bfs:      1) search 'd' into all the neighbors of node 'b'
+
+	2) search 'd' into all the neighbors of the neighbors of 'b' one by one
+
+Best conceivable runtime: O ( n )
+
+Time complexity: O ( n )
+Space complexity: O ( n ) - because of the 'state' attribute assuming that we have added it
+"""
+
+def search ( graph, start, end ):
+
+	if ( start == end ):
+		return True
+
+	q = LinkedList ( )
+
+	for i in graph.getNodes ( ):
+		i.state = 'unvisited'
+
+	start.state = 'visiting'
+
+	q.add ( start )
+
+	while ( !q.isEmpty ( ) ):
+		u = q.removeHead ( )
+		if ( u is not None ):
+			for v in u.getAdjacent ( ):
+				if ( v.state == 'unvisited' ):
+					if ( v == end ):
+						return True
+					else:
+						v.state = 'visiting'
+						q.add ( v )
+		u.state = 'visited'
+	return False
