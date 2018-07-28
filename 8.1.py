22bc5c4d09f109bd04f55f589058edd5c53772a9
Harish Jhaldiyal <31455549+harishjhaldiyal@users.noreply.github.com>
2018-07-28 16:56:26 +0530

    8.1 Update

--- 8.1.py
+++ 8.1.py
@@ -71,6 +71,17 @@ def userInterface ( n ):
 		print ( "Invalid input" )
 		return
 
+def iterativeSolution ( n ):
+
+	memo = [ None ] * ( n + 1 )
+	memo [ 0 ] = 1
+	memo [ 1 ] = 1
+	memo [ 2 ] = 2
+	for i in range ( 3, n+1 ):
+		memo [ i ] = memo [ i - 1 ] + memo [ i - 2 ] + memo [ i - 3 ]
+	return memo [ n ]
+
+
 
 
 
